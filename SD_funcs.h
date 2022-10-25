b9cf6a5669b5b27d766963221ac10b2cfd3a31e6
nmpacholski <97474106+nmpacholski@users.noreply.github.com>
2022-10-25 13:55:05 -0400

    Create SD_funcs.h

---
+++ SD_funcs.h
@@ -0,0 +1,19 @@
+#ifndef _SD_FUNCS_H_
+#define _SD_FUNCS_H_
+
+#include "FS.h"
+
+void createDir(fs::FS &fs, const char * path);
+void removeDir(fs::FS &fs, const char * path);
+
+void readFile(fs::FS &fs, const char * path);
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char * path, const char * message);
+void renameFile(fs::FS &fs, const char * path1, const char * path2);
+void deleteFile(fs::FS &fs, const char * path);
+
+void testFileIO(fs::FS &fs, const char * path);
+
+void loop_SD();
+
+#endif
