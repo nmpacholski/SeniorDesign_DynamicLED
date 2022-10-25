54530ca006b82e15c73cc31bc68332dde9547c75
nmpacholski <97474106+nmpacholski@users.noreply.github.com>
2022-10-25 14:16:31 -0400

    Create sd_defines.h

---
+++ SD_folder/dont_touch/sd_defines.h
@@ -0,0 +1,12 @@
+#ifndef _SD_DEFINES_H_
+#define _SD_DEFINES_H_
+
+typedef enum {
+    CARD_NONE,
+    CARD_MMC,
+    CARD_SD,
+    CARD_SDHC,
+    CARD_UNKNOWN
+} sdcard_type_t;
+
+#endif /* _SD_DISKIO_H_ */
