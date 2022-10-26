d275b94596b5c22b1c7b6bf0da3ab2f50517bf0f
Pun <parunsir@purdue.edu>
2022-10-25 21:30:35 -0400

    Add gif function

--- image_conversion.py
+++ image_conversion.py
@@ -40,11 +40,12 @@ def scale32(imagearray, width, heigth):
     print(newimg, file = f)
     f.close()
     newimg = np.array(newimg)
-    im = Image.fromarray(newimg) #Save array into a new image file
-    im.save("result.bmp")
+    #print(newimg)
+    #im = Image.fromarray(newimg) #Save array into a new image file
+    #im.save("result.bmp")
     print('Success')
     
-    return()
+    return(newimg)
 
 #Code from: https://www.daniweb.com/programming/software-development/threads/253957/converting-an-image-file-png-to-a-bitmap-file
 def pngtobmp(imagearray): 
@@ -91,8 +92,11 @@ def cropimg(imagearray, width, height):
     cropimage = np.array(cropimage)
     cropimage = np.array_split(cropimage, targetdimension) #Reshape the array to be the target final dimension
     cropimage = np.array(cropimage)
-    x = Image.fromarray(cropimage)
-    x.save("cropresult.bmp")
+    y = cropimage[0,0,0]
+    #print(y)
+    #print(type(y))
+    #x = Image.fromarray(cropimage)
+    #x.save("cropresult.bmp")
     print('Crop Success')
     return(cropimage) #Return the cropped image to main
         
@@ -100,25 +104,60 @@ def cropimg(imagearray, width, height):
 np.set_printoptions(threshold=sys.maxsize)
 n = 0
 crop = 0
+numframe = 1
+gifcheck = 0
+test = []
+framelimit = 9
 while n == 0:
     filename = input('Enter filename: ') #Ask user for the name of the image file
-    if (filename[-4:] == '.png') or (filename[-4:] == '.ico') or (filename[-4:] == '.jpg') or (filename[-4:] == '.bmp'):
+    if (filename[-4:] == '.png') or (filename[-4:] == '.ico') or (filename[-4:] == '.jpg') or (filename[-4:] == '.bmp') or (filename[-4:] == '.gif'):
         n = 1
     else:
-        print('Wrong image format. Program only support .png, .ico, .jpg, .bmp')
+        print('Wrong image format. Program only support .png, .ico, .jpg, .bmp, .gif')
 inputfile = Image.open(filename)
-if (filename[-4:] == '.png') or (filename[-4:] == '.ico'): 
-    inputfile = pngtobmp(inputfile) #Convert .png and .ico to .bmp file
-img = np.array(inputfile)
-[height,width,d] = img.shape
-#print(img.shape)
-if width != height:
-    print("Image doesn't have the same width and height, would you like to crop the image to prevent image stretching")
-    crop = input('1 for Yes, 0 for No: ') #Ask user if they want to crop the image
-if crop == '1':
-    img = cropimg(img, width, height) #Crop the image
+'''if (filename[-4:] == '.png') or (filename[-4:] == '.ico'): 
+    inputfile = pngtobmp(inputfile) #Convert .png and .ico to .bmp file'''
+if (filename[-4:] == '.gif'):
+    numframe = inputfile.n_frames
+    gifcheck = 1
+    print(numframe)
+    if numframe > framelimit:
+        print('The ESP32 can only show 8 frames of animation due to memory issue')
+        startingframe = input('Please choose the starting frame: ')
+        endingframe = input('Please choose the ending frame: ')
+for i in range(0,numframe,1):
+    if gifcheck == 1:
+        if i >= numframe - 1:
+            continue
+        print('current seek =', i+1)
+        inputfile.seek(i + 1)
+    #print('continue check')
+    img = np.array(inputfile)
     [height,width,d] = img.shape
-scale32(img, width, height) #Scale the image to 32x32 resolution and save it to the new file
+    if d == 4:
+        tempimg = Image.fromarray(img)
+        fiximg = pngtobmp(tempimg)
+        img = np.array(fiximg)
+    if i == 0:
+        if width != height:
+            print("Image doesn't have the same width and height, would you like to crop the image to prevent image stretching")
+            crop = input('1 for Yes, 0 for No: ') #Ask user if they want to crop the image
+    if crop == '1':
+        img = cropimg(img, width, height) #Crop the image
+        [height,width,d] = img.shape
+    newimg = scale32(img, width, height) #Scale the image to 32x32 resolution and save it to the new file
+    if i == 0:
+        final = newimg
+    else:
+        final = np.concatenate((final, newimg), axis = 0)
 
+print(final.shape)
+f = open("result.txt", "w")
+final = final.tolist()
+print(final, file = f)
+f.close()
+final = np.array(final, dtype=np.uint8)
+im = Image.fromarray(final) #Save array into a new image file
+im.save("result.bmp")
 
 
